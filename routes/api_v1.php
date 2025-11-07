2b0cffa23e291a0a5e1ba4116da9e472f6960b91
rochev <gitarar98@yandex.ru>
2025-11-07 19:15:22 +0500

    adding api routes

---
+++ routes/api_v1.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/auth')
+    ->name('auth.')
+    ->group(base_path('routes/v1/auth.php'));
+
+Route::prefix('/profile')
+    ->name('profile.')
+    ->middleware('auth:sanctum')
+    ->group(base_path('routes/v1/profile.php'));
+
+Route::prefix('/public')
+    ->name('public.')
+    ->middleware('auth:sanctum')
+    ->group(base_path('routes/v1/public.php'));
