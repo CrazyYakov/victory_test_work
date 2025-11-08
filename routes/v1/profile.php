ae7a92c8541df0424a017d77e433df3ad9412dd8
rochev <gitarar98@yandex.ru>
2025-11-08 11:13:47 +0500

    adding create order for  profile domain

--- routes/v1/profile.php
+++ routes/v1/profile.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use OrderManagement\Profile\Presentation\Controllers\V1\CreateOrderController;
+
+Route::prefix('/orders')
+    ->name('orders.')
+    ->group(function () {
+        Route::post('/', CreateOrderController::class)
+            ->name('store');
+    });
