08e817c95b8aa25ed7c725957b9b7c1046125cbc
rochev <gitarar98@yandex.ru>
2025-11-08 12:13:02 +0500

    adding routes for index and show order

--- routes/v1/profile.php
+++ routes/v1/profile.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use OrderManagement\Profile\Presentation\Controllers\V1\CreateOrderController;
+use OrderManagement\Profile\Presentation\Controllers\V1\IndexOrderController;
+use OrderManagement\Profile\Presentation\Controllers\V1\ShowOrderController;
 
 Route::prefix('/orders')
     ->name('orders.')
     ->group(function () {
         Route::post('/', CreateOrderController::class)
             ->name('store');
+
+        Route::get('/', IndexOrderController::class)
+            ->name('index');
+
+        Route::get('/{order}', ShowOrderController::class)
+            ->name('show');
     });
